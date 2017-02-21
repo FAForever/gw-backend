d8418d3555dda9b2eae2f6b1111f20e035178ec6
Christian Schmidt <c-schmidt@gmx.eu>
2017-02-21 18:19:20 +0100

    Add user choice to test client

--- src/test/java/com/faforever/gw/model/ValidationHelperTest.java
+++ src/test/java/com/faforever/gw/model/ValidationHelperTest.java
@@ -2,8 +2,12 @@ package com.faforever.gw.model;
 
 import com.faforever.gw.bpmn.task.InitiateAssaultTask;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
 
 import java.util.List;
 
@@ -19,6 +23,7 @@ public class ValidationHelperTest {
         validationHelper = new ValidationHelper();
     }
 
+    @Test
     public void validateCharacterInBattle__ExpectTrue_Success() {
         GwCharacter character = mock(GwCharacter.class);
         Battle battle = mock(Battle.class);
@@ -27,5 +32,35 @@ public class ValidationHelperTest {
         when(character.getBattleParticipantList()).thenReturn(battleParticipantList);
 
          validationHelper.validateCharacterInBattle(character, battle, true);
+    }
+
+    @Test
+    public void generateTestUser() {
+        MacSigner macSigner = new MacSigner("secret");
+
+        // {"user_id": 1, "user_name": "UEF Alpha", "authorities":["ROLE_USER"], "exp": 4102444740}
+        // {"user_id": 2, "user_name": "UEF Bravo", "authorities":["ROLE_USER"], "exp": 4102444740}
+        // {"user_id": 3, "user_name": "Cybran Charlie", "authorities":["ROLE_USER"], "exp": 4102444740}
+        String tokenData = "{\"user_id\": 2, \"user_name\": \"UEF Bravo\", \"authorities\":[\"ROLE_USER\"], \"exp\": 4102444740}";
+        Jwt token = JwtHelper.encode(tokenData, macSigner);
+        String stringToken = token.getEncoded();
+        System.out.println(stringToken);
+        ;
+//    public static User fromJwtToken(String stringToken) {
+//        try {
+//            Jwt token = JwtHelper.decodeAndVerify(stringToken, new MacSigner("secret"));
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            JsonNode data = objectMapper.readTree(token.getClaims());
+//
+//            List<String> authorities = objectMapper.readerFor(new TypeReference<List<String>>(){}).readValue(data.get("authorities"));
+//            List<SimpleGrantedAuthority> grantedAuthorities = new ArrayList<>();
+//
+//            authorities.forEach(role -> grantedAuthorities.add(new SimpleGrantedAuthority(role)));
+//
+//            return new User(data.get("user_id").asLong(), data.get("user_name").asText(), stringToken, grantedAuthorities);
+//        }
+//        catch(Exception e) {
+//            throw new RuntimeException("user not authorized");
+//        }
     }
 }
