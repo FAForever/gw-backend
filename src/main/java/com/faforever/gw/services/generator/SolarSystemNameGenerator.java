82c9531b4af2de8e636f821c3e8cff746f1e9be5
Alastair White <ally@hvitt.co.uk>
2017-09-25 19:34:41 +0100

    Completed a passable SolarSystemNameGenerator
    Added a couple of extra options to the constellations and added a list of other prefixes (gathered from various surveys). Tidied up the next() function and refactored the name generation into separate function and added generateNames(int amount) and clear() which returns a given amount of star system names and clears the current set of names respectively.

--- src/main/java/com/faforever/gw/services/generator/SolarSystemNameGenerator.java
+++ src/main/java/com/faforever/gw/services/generator/SolarSystemNameGenerator.java
@@ -9,40 +9,78 @@ import org.springframework.stereotype.Service;
 public class SolarSystemNameGenerator {
     private Random rand = new Random();
 	private Set<String> names = new HashSet<String>();
-    private long index = 1;
     
     
-    private String[] greekLetters = { "Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota",
+    private final static String[] greekLetters = { "Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota",
 			"Kappa", "Lambda", "Mu", "Nu", "Xi", "Omicron", "pi", "Rho", "Sigma", "Tau", "Upsilon", "Phi", "Chi", "Psi",
 			"Omega" };
 
-	private String[] constellations = { "Andromedae", "Antilae", "Apodis", "Aquarii", "Aquilae", "Arae", "Arietis",
-			"Aurigae", "Boötis", "Camelopardis", "Canis Majoris", "Canis Minoris", "Eridani", "Herculis", "Leonis",
+	private final static String[] constellations = { "Andromedae", "Antilae", "Apodis", "Aquarii", "Aquilae", "Arae", "Arietis",
+			"Aurigae", "Boötis", "Camelopardis", "Canis Majoris", "Canis Minoris", "Draconis", "Eridani", "Gemini", "Herculis", "Leonis",
 			"Lyrae", "Octanis", "Orionis", "Pegasi", "Persei", "Phoenicis", "Ursae Majoris", "Ursae Minoris",
 	"Volantis" };
 	
-	private String[] astronomers = { "Galilea", "Copernicus", "Kepler", "Hubble", "Tyson", "Ptolemy", "Sagan",
+	private final static String[] astronomers = { "Galilea", "Copernicus", "Kepler", "Hubble", "Tyson", "Ptolemy", "Sagan",
 			"Herschel", "Halley", "Messier", "Kuiper", "Laplace", "Brahe", "Huygens", "Cassini"};
 
-    public String next() {
-		//Proportion the names 6/10 greek/constellation, 3/10 after astronomers and 1/10 based on Survey.
+	
+	private final static String[] otherPrefixes = {"NGC", "HR", "WNC", "SSDS", "V", "WISE", "LQ", "DENIS-P", "HIP", "HD"};
+
+    	// Clears the set of previous names
+	public void clear() {
+		currentNames.clear();
+	}
+	
+	// Generate a given amount of solar system names in batch
+	public String[] generateNames(int amount) {
+		String[] names = new String[amount];
+		for (int i=0;i<amount;i++) {
+			names[i] = next();
+		}
+
+		return names;
+	}
+	
+	// Generate another solar system name
+	public String next() {
+		// Proportion the names 6/10 Greek/constellation, 3/10 after astronomers and 1/10 based on astronomical surveys.
 		int val = rand.nextInt(10);
 		
 		if (val < 6) {
-			String name = greekLetters[rand.nextInt(greekLetters.length)] + " " +  constellations[rand.nextInt(constellations.length)];
-			while (names.contains(name) == true){
-				name = greekLetters[rand.nextInt(greekLetters.length)] + " " +  constellations[rand.nextInt(constellations.length)];
-			}
-			return name;
+			return greekName();
 		}
-		if (val < 9) {
-			String name = astronomers[rand.nextInt(astronomers.length)] + " " + rand.nextInt(40);
-			while (names.contains(name) == true) {
-				name = astronomers[rand.nextInt(astronomers.length)] + " " + rand.nextInt(40);
-			}
-			return name;
+		else if (val < 9) {
+			return astroName();
 		}
-
-		return "solar system #" + index++; // FIXME: Implement actual logic
+		else {
+			return otherName();			
+		}
+	}
+	
+	private String greekName() {
+		String name = greekLetters[rand.nextInt(greekLetters.length)] + " " +  constellations[rand.nextInt(constellations.length)];
+		while (currentNames.contains(name) == true){
+			name = greekLetters[rand.nextInt(greekLetters.length)] + " " +  constellations[rand.nextInt(constellations.length)];
+		}
+		currentNames.add(name);
+		return name;
+	}
+	
+	private String astroName() {
+		String name = astronomers[rand.nextInt(astronomers.length)] + " " + (rand.nextInt(80) + 1); 
+		while (currentNames.contains(name) == true) {
+			name = astronomers[rand.nextInt(astronomers.length)] + " " + (rand.nextInt(80) + 1);
+		}
+		currentNames.add(name);
+		return name;
+	}
+	
+	private String otherName() {
+		String name = otherPrefixes[rand.nextInt(otherPrefixes.length)] + " " + rand.nextInt(10000) + 1;
+		while (currentNames.contains(name) == true) {
+			name = otherPrefixes[rand.nextInt(otherPrefixes.length)] + " " + rand.nextInt(10000) + 1;
+		}
+		currentNames.add(name);
+		return name;
 	}
 }
