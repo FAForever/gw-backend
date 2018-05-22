3fd3a7e39bf7d8e242044e2ef71750cdd807514b
Brutus5000 <Brutus5000@gmx.net>
2018-05-23 00:12:52 +0200

    Ask client game to start without showing the lobby

--- src/main/java/com/faforever/gw/messaging/lobby/outbound/CreateMatchRequest.java
+++ src/main/java/com/faforever/gw/messaging/lobby/outbound/CreateMatchRequest.java
@@ -1,5 +1,6 @@
 package com.faforever.gw.messaging.lobby.outbound;
 
+import com.faforever.gw.messaging.lobby.LobbyMode;
 import com.faforever.gw.model.Faction;
 import lombok.*;
 
@@ -15,13 +16,14 @@ public class CreateMatchRequest extends OutboundLobbyMessage {
     private int map;
     private String featuredMod;
     private List<Participant> participants;
+    private LobbyMode lobbyMode = LobbyMode.NONE;
 
     @Getter
     @Setter
     public static class Participant {
         private long id;
         private Faction faction;
-        private int slot;
+        private int startSpot;
         private int team;
         private String name;
     }
