e61b967d956731ff9f9abedc5163f2f3c1186c62
Geosearchef <geosearchef@googlemail.com>
2018-12-19 21:52:16 +0100

    Refactor token extraction to also read bearer tokens.

--- src/main/java/com/faforever/gw/config/oauth2/QueryParameterTokenExtractor.java
+++ src/main/java/com/faforever/gw/config/oauth2/QueryParameterTokenExtractor.java
@@ -1,48 +1,24 @@
 package com.faforever.gw.config.oauth2;
 
-import com.faforever.gw.config.GwServerProperties;
-import com.faforever.gw.security.JsonAccessToken;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.jwt.Jwt;
-import org.springframework.security.jwt.JwtHelper;
-import org.springframework.security.jwt.crypto.sign.MacSigner;
-import org.springframework.security.oauth2.provider.authentication.TokenExtractor;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
+import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 import static org.springframework.security.oauth2.common.OAuth2AccessToken.ACCESS_TOKEN;
 
 @Slf4j
 @Component
-public class QueryParameterTokenExtractor implements TokenExtractor {
-    private final ObjectMapper objectMapper;
-    private final MacSigner macSigner;
-
-    public QueryParameterTokenExtractor(GwServerProperties properties, ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-        this.macSigner = new MacSigner(properties.getJwt().getSecret());
-    }
+public class QueryParameterTokenExtractor extends BearerTokenExtractor {
 
     @Override
-    public Authentication extract(HttpServletRequest request) {
+	public String extractToken(HttpServletRequest request) {
         String accessTokenString = request.getParameter(ACCESS_TOKEN);
         if (accessTokenString == null) {
-            return null;
+			return super.extractToken(request);
         }
 
-        try {
-            Jwt jwt = JwtHelper.decodeAndVerify(accessTokenString, macSigner);
-            JsonAccessToken accessToken = objectMapper.readValue(jwt.getClaims(), JsonAccessToken.class);
-            return new PreAuthenticatedAuthenticationToken(accessTokenString, null);
-        } catch (IOException e) {
-            log.error("JWT could not be mapped to JsonAccessToken - probably malformed", e);
-            throw new PreAuthenticatedCredentialsNotFoundException("No access token in connection string");
-        }
+		return accessTokenString;
     }
 }
