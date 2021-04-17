ae2fa2ddbf673c5a20358551e04f8a9058c0ba50
Brutus5000 <Brutus5000@gmx.net>
2021-04-17 10:33:43 +0200

    Fix timeout on character creation #2

--- src/test/java/com/faforever/gw/bpmn/CharacterCreationTest.java
+++ src/test/java/com/faforever/gw/bpmn/CharacterCreationTest.java
@@ -2,6 +2,7 @@ package com.faforever.gw.bpmn;
 
 import com.faforever.gw.bpmn.services.CharacterCreationService;
 import com.google.common.collect.ImmutableMap;
+import org.camunda.bpm.engine.impl.util.ClockUtil;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
@@ -97,16 +98,51 @@ public class CharacterCreationTest {
                 "IntermediateThrowEvent_SendNameProposal"
 
         )
-                .isWaitingAt("IntermediateCatchEvent_ReceiveNameSelection");
+                .isWaitingAt("ReceiveTask_ReceiveNameSelection");
 
         processEngineRule.getRuntimeService().correlateMessage(CharacterCreationService.RECEIVE_CHARACTER_NAME_SELECTION_MESSAGE, "test");
 
         assertThat(processInstance).hasPassedInOrder(
                 "IntermediateThrowEvent_SendNameProposal",
-                "IntermediateCatchEvent_ReceiveNameSelection",
+                "ReceiveTask_ReceiveNameSelection",
                 "ServiceTask_CreateCharacter",
                 "BoundaryEvent_Error_InvalidSelection",
                 "EndEvent_Error_InvalidSelection"
+        )
+                .isEnded();
+    }
+
+    @Test
+    @Deployment(resources = "bpmn/character_creation.bpmn")
+    public void errorTimeoutOnNameSelection() throws Exception {
+        DelegateExpressions.registerJavaDelegateMock("checkUserCharactersTask")
+                .onExecutionSetVariables(ImmutableMap.of("hasActiveCharacter", false, "hasDeadCharacter", true, "factionMatches", true));
+        DelegateExpressions.registerJavaDelegateMock("createCharacterTask").onExecutionThrowBpmnError("3003", "Your chosen character name was not offered");
+
+
+        ProcessInstance processInstance = startProcess();
+
+        assertThat(processInstance).hasPassedInOrder(
+                "StartEvent_UserRequestsCharacter",
+                "ServiceTask_CheckActiveAndDeadCharacters",
+                "ExclusiveGateway_HasActiveCharacter",
+                "ExclusiveGateway_HasDeadCharacter",
+                "ExclusiveGateway_FactionMatches",
+                "ExclusiveGateway_MergeGenerate",
+                "ServiceTask_GenerateProposal",
+                "IntermediateThrowEvent_SendNameProposal"
+
+        )
+                .isWaitingAt("ReceiveTask_ReceiveNameSelection");
+
+        ClockUtil.offset(60 * 11 * 1000L);
+        execute(job());
+
+        assertThat(processInstance).hasPassedInOrder(
+                "IntermediateThrowEvent_SendNameProposal",
+                "ReceiveTask_ReceiveNameSelection",
+                "BoundaryEvent_Error_Timeout",
+                "EndEvent_Error_Timeout"
         )
                 .isEnded();
     }
@@ -130,13 +166,13 @@ public class CharacterCreationTest {
                 "IntermediateThrowEvent_SendNameProposal"
 
         )
-                .isWaitingAt("IntermediateCatchEvent_ReceiveNameSelection");
+                .isWaitingAt("ReceiveTask_ReceiveNameSelection");
 
         processEngineRule.getRuntimeService().correlateMessage(CharacterCreationService.RECEIVE_CHARACTER_NAME_SELECTION_MESSAGE, "test");
 
         assertThat(processInstance).hasPassedInOrder(
                 "IntermediateThrowEvent_SendNameProposal",
-                "IntermediateCatchEvent_ReceiveNameSelection",
+                "ReceiveTask_ReceiveNameSelection",
                 "ServiceTask_CreateCharacter")
                 .isWaitingAtExactly("IntermediateThrowEvent_Ack");
 
